5962727209125b128a8afce73eddb6ad22e86842
Akim <akimkom@protonmail.com>
2023-07-28 22:08:42 +0300

    Reduced the size of previews

--- gen-previews.sh
+++ gen-previews.sh
@@ -5,16 +5,17 @@ dir="./previews"
 rm $dir/*
 
 echo "Generating previews..."
-convert -density 225 ./Cultivated\ Code\ of\ Conduct.pdf $dir/CCC.jpg
+convert -density 225 -quality 10 ./Cultivated\ Code\ of\ Conduct.pdf $dir/CCC.jpg
 
 # Create an array of generated preview files.
 previews=($(ls $dir))
 
-# Delete the lines between the tags
+# Delete the lines between the tags.
 sed -i '/<!--PREVIEWS START-->/,/<!--PREVIEWS FINISH-->/ {//!d}' ./README.md
 
 for ((i=${#previews[@]}-1 ; i>=0; i--)); do
     md_preview="[![The preview of the CCC]($dir/${previews[i]})](./Cultivated%20Code%20of%20Conduct.pdf)"
+    # Insert a linked image after the start tag.
     awk -v n="$md_preview" '/<!--PREVIEWS START-->/ {print; print n; next}1' ./README.md > tmp && mv tmp README.md
     echo ${previews[i]}
 done
